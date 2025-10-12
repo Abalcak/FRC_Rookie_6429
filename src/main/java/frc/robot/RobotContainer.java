25401f5b536e2a9c52ee8b559b24381aee0942e1
LogicalHolism <67659592+LogicalHolism@users.noreply.github.com>
2025-10-12 23:35:27 +0300

    contain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,19 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.commands.intake.IntakeOnCommand;
+import frc.robot.commands.intake.IntakeShootCommand;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,17 +25,23 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  
+  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  
+  private final CommandXboxController joystick = new CommandXboxController(0);
+  private final Joystick operator = new Joystick(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final JoystickButton intake = new JoystickButton(operator, 0);
+  private final JoystickButton shoot = new JoystickButton(operator, 0);
+  
+
   public RobotContainer() {
-    // Configure the trigger bindings
+    NamedCommands.registerCommand("intake", new IntakeOnCommand(intakeSubsystem));
+    NamedCommands.registerCommand("shoot", new IntakeShootCommand(intakeSubsystem));
+
     configureBindings();
+
   }
 
   /**
@@ -42,13 +54,11 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    
+    // Hareket etme definelanmadı
+    
+    intake.whileTrue(new IntakeOnCommand(intakeSubsystem));
+    shoot.whileTrue(new IntakeShootCommand(intakeSubsystem));
   }
 
   /**
